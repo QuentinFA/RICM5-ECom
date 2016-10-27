81210c8874b119c1bdb1cd44fb16b98f810e7850
ccenyo <medewou@gmail.com>
2016-10-27 15:40:44 +0200

    correction de AbstractFacade

--- ECOM_EJB/ejbModule/session/AbstractFacade.java
+++ ECOM_EJB/ejbModule/session/AbstractFacade.java
@@ -9,11 +9,7 @@ import java.util.List;
 
 import javax.ejb.Remote;
 import javax.persistence.EntityManager;
-<<<<<<< HEAD
-=======
 
-
->>>>>>> 14edeb68d32926fb49e83deb7ea070fa00d7a7ff
 @Remote
 public abstract class AbstractFacade<T> { 
 
