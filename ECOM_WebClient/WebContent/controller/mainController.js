4413f8afc6eb9ca0096ab8e33cecb05a88e0ff58
ghamm03 <hammouti.guillaume@gmail.com>
2016-11-15 12:31:01 +0100

    update controller angular

--- ECOM_WebClient/WebContent/controller/mainController.js
+++ ECOM_WebClient/WebContent/controller/mainController.js
@@ -2,8 +2,14 @@ var myApp = angular.module('myApp', []);
 
 myApp.controller('MainCtrl', ['$scope','$http', function ($scope,$http) {
 	
-	$scope.annonces = {};
-	$scope.title = [];
+	$scope.annonces = [];
+	$scope.nbAnnonces = 4;
+    $scope.nbPages = $scope.nbAnnonces /2;
+
+    $scope.getNumber = function(num) {
+        return new Array(num);   
+    }
+
     $http.get('./annonces.json')
         .success(function(data) {
             $scope.annonces=data;
