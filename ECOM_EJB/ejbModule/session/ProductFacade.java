4ae98efa4f129960448bdbf6e7d5e86aa8dafeed
赵子龙 <zilong_zhao@foxmail.com>
2016-11-19 16:29:03 +0100

    upload images to S3

--- ECOM_EJB/ejbModule/session/ProductFacade.java
+++ ECOM_EJB/ejbModule/session/ProductFacade.java
@@ -48,4 +48,10 @@ public class ProductFacade extends AbstractFacade<Product> {
         return (List<Product>) query.getResultList();
       }
     
+    @SuppressWarnings("unchecked")
+	public List<Product> findProductByIdUserAndTitle(String idUser, String title) {
+        Query query = em.createQuery("SELECT p FROM Product p where p.idUser = '"+idUser+"' AND p.title = '"+title+"' ");
+        return (List<Product>) query.getResultList();
+      }
+    
 }
